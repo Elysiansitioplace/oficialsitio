e219e15a9cc744af31ef33d1519bfffec37b9687
Elysiansitioplace <154612607+Elysiansitioplace@users.noreply.github.com>
2025-01-15 03:27:25 -0300

    Add files via upload

---
+++ js/comentario.js
@@ -0,0 +1,7 @@
+function comentario(){
+    document.querySelector('.fundo-addcomentarios').style.display = "flex";
+}
+
+function fecharcoment(){
+    document.querySelector('.fundo-addcomentarios').style.display = "none";
+}
